ad41c7ab148a55ef1b4755f64745684306755985
Aaron White <awhite@tarvold.com>
2019-10-15 18:56:10 -0700

    Added axios and updated posting code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () => {
+  useEffect(() => {
+    post()
+  }, [])
+  async function post() {
+    const { data } = await axios.post(`http://localhost:3000/`, {
+      'name': 'bill'
+    })
+    console.log(data)
+  }
+  return (<div className="App" />)
 }
 
-export default App;
+
+
