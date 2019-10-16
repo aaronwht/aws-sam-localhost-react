5a77a4898414b8d7fc1bf1e90f9cb59260d5c395
Aaron White <awhite@tarvold.com>
2019-10-16 15:17:17 -0700

    Pulled test data from end point to demonstrate this is working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default () => {
+  const [testData, setTestData] = useState()
+   
   useEffect(() => {
-    post()
-  }, [])
-  async function post() {
+   (async () => {
+     getData()
+     postData()
+   })()
+ }, [])
+  
+  async function getData() {
+    const { data } = await axios.get(`http://localhost:3030/test`)
+    setTestData(data) 
+  }
+
+  async function postData() {
     const { data } = await axios.post(`http://localhost:3030/`, {
       'name': 'bill'
     })
     console.log(data)
   }
-  return (<div className="App" />)
+  return (<div className="App">{testData}</div>)
 }
 
 
